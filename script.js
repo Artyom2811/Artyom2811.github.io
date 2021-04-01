ecddaef7a0dc39836c6500650598fbeaad32f878
beltik <Artyombelt@gmail.com>
2021-04-01 22:02:39 +0300

    add autoHeight for BackButton

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+{
+var heightOfBackButton = document.getElementsByClassName("bodyStyle").item(0).offsetHeight
+document.getElementsByClassName("backButton").item(0).style.height = heightOfBackButton  + 'px'
+}
+
 function showMessage() {
   alert( 'Всем привет!' );
 }
+
+
 //Пример вызова из HTML
 //<a href="#" onclick="showMessage()">click me</a>
