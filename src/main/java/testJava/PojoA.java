ca192dd972b3f182db095e2abc8d49eeac5e656a
Artyom2811 <Rfrdctckj;yj4252>
2021-07-26 09:49:39 +0300

    add new data

---
+++ src/main/java/testJava/PojoA.java
@@ -0,0 +1,11 @@
+package testJava;
+
+public class PojoA {
+    String firtName;
+    String lastName;
+
+    public PojoA(String firtName, String lastName) {
+        this.firtName = firtName;
+        this.lastName = lastName;
+    }
+}
