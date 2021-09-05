5080c195f6cbe3a17687b5122ad73093adb5f9e7
Artyom2811 <Rfrdctckj;yj4252>
2021-09-05 18:30:18 +0300

    add new data

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 {
+//Задание высоты для BackButton в зависимости от размера контента
+//Получение высоты bodyStyle
 var heightOfBackButton = document.getElementsByClassName("bodyStyle").item(0).offsetHeight
+//Получение высоты экрана клиента
+var heightOfScreen = document.documentElement.clientHeight
+if (heightOfBackButton > heightOfScreen) {
 document.getElementsByClassName("backButton").item(0).style.height = heightOfBackButton  + 'px'
+} else {
+  document.getElementsByClassName("backButton").item(0).style.height = heightOfScreen  + 'px'
+}
 }
 
 function showMessage() {
   alert( 'Всем привет!' );
+}
+
+function getBodyHeight() {
+  console.log(window.getComputedStyle(document.getElementsByClassName("bodyStyle").item(0)).height);
 }
 
 
