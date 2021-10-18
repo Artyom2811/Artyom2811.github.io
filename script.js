87bc4de4342c9f407a2b001059e2a63df367f936
Artyom Beltiuhov <artyom.beltiuhov@macq.eu>
2021-10-18 13:27:10 +0300

    add and changed new data

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 function setBackButtonHeight() {
-//Задание высоты для BackButton в зависимости от размера контента
-//Получение высоты bodyStyle
-var heightOfBackButton = document.getElementsByClassName("bodyStyle").item(0).offsetHeight
-//Получение высоты экрана клиента
-var heightOfScreen = document.documentElement.clientHeight
-if (heightOfBackButton > heightOfScreen) {
-document.getElementsByClassName("backButton").item(0).style.height = heightOfBackButton  + 'px'
-} else {
-  document.getElementsByClassName("backButton").item(0).style.height = heightOfScreen  + 'px'
-}
+  //Задание высоты для BackButton в зависимости от размера контента
+  //Получение высоты bodyStyle
+  var heightOfBackButton = document.getElementsByClassName("bodyStyle").item(0).offsetHeight
+
+  //Получение высоты экрана клиента
+  var heightOfScreen = document.documentElement.clientHeight
+
+  if (heightOfBackButton > heightOfScreen) {
+  document.getElementsByClassName("backButton").item(0).style.height = heightOfBackButton  + 'px'
+  } else {
+    document.getElementsByClassName("backButton").item(0).style.height = heightOfScreen  + 'px'
+  }
 }
 
 function showMessage() {
@@ -18,7 +20,6 @@ function showMessage() {
 function getBodyHeight() {
   console.log(window.getComputedStyle(document.getElementsByClassName("bodyStyle").item(0)).height);
 }
-
 
 //Пример вызова из HTML
 //<a href="#" onclick="showMessage()">click me</a>
