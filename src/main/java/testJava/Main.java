bf0e5e6f38c41498c0dc2d487078baf62d826d0e
Artyom2811 <Rfrdctckj;yj4252>
2021-12-31 21:20:30 +0200

    added and changed data

--- src/main/java/testJava/Main.java
+++ src/main/java/testJava/Main.java
@@ -1,8 +1,12 @@
 package testJava;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
     }
 }
