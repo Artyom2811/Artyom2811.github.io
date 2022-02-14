a525d9b50b4cb6acec4acb7d0e1df202a6a87029
artyom2811 <artyombelt@gmail.com>
2022-02-14 20:13:52 +0200

    added and changed data

--- src/main/java/testJava/Main.java
+++ src/main/java/testJava/Main.java
@@ -4,6 +4,12 @@ import java.io.IOException;
 
 public class Main {
 
+    String f = "123";
+
     public static void main(String[] args) throws IOException {
+    }
+
+    public void a() {
+
     }
 }
