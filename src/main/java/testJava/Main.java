fb9caaee4f0e012ed08dc3e645df1c420dd7baab
Artyom2811 <Rfrdctckj;yj4252>
2022-06-29 01:58:01 +0300

    added and changed data

--- src/main/java/testJava/Main.java
+++ src/main/java/testJava/Main.java
@@ -7,6 +7,11 @@ public class Main {
     String f = "123";
 
     public static void main(String[] args) throws IOException {
+
+        int[] a = {1,2,3};
+        for (int i:a) {
+            System.out.println(i);
+        }
     }
 
     public void a() {
