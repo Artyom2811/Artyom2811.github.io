8410d8d150c3b01058074b9903c763d5d7c94d42
Artyom2811 <Rfrdctckj;yj4252>
2022-09-26 02:10:43 +0300

    added and changed data

--- src/main/java/testJava/Main.java
+++ src/main/java/testJava/Main.java
@@ -1,9 +1,13 @@
 package testJava;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        List<String> list = List.of("Hello", "world");
+        var list2 = list.stream().reduce((a, b) -> a + " " + b);
+        System.out.println(list2.get());
 
     }
 }
